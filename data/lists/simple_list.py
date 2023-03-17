2f498c648dc055206605fa04ac1fe479b910849a
LucaBaldassano <luca.baldassano@icloud.com>
2023-03-17 16:04:29 +0000

    Added code for a Simple list

---
+++ data/lists/simple_list.py
@@ -0,0 +1,8 @@
+def directions():
+    directions_list = ["Move Forward", "Move Backward", "Turn Left", "turn Right"]
+    return directions_list
+
+def run():
+    directions_list = directions()
+    print(directions_list)
+run()
